865091c6be08d0c384189f2c3a3580e7038364c8
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-09-30 22:04:41 +0700

    feat: add user-profile page (self view)

---
+++ app/views/user-profile/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/user-profile.css">
+    <title>My Profile</title>
+</head>
+<body>
+<div class="card">
+
+  <div class="ds-top"></div>
+  <div class="avatar-holder">
+    <img src="../../../public/img/logo.jpg" alt="Albert Einstein">
+  </div>
+  <div class="ds-level">
+    <h6>LEVEL X</i></h6>
+    <div class="skill html">
+      <h6>95/100</h6>
+      <div class="bar bar-level">
+        <p>95%</p>
+      </div>
+    </div>
+  </div>
+  <div class="name">
+    <a href="" target="_blank">Dummy Name Dummy Name</a>
+    <h6 title="Username"> DummyUsername</h6>
+  </div>
+  <div class="button">
+    <a href="#" class="btn">Edit </a>
+  </div>
+  <div class="ds-info">
+    <div class="ds achievements">
+      <h6 title="Total achievements">Awards</h6>
+      <p>29</p>
+    </div>
+    <div class="ds quest">
+      <h6 title="Total quest completed">Quest </h6>
+      <p>0</p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
