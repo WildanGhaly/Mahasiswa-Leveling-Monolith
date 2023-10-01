46d5205b1553d79aad39aaf7ade793d9fc11d3f1
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-01 21:24:38 +0700

    feat: add edit profile FE and design TODO: Javascript for ajax

---
+++ app/views/edit-profile/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/edit-profile.css">
+    <script src="../../../public/js/edit-profile.js"></script>
+    <title>My Profile</title>
+</head>
+<body>
+<div class="card">
+  <div class="ds-top"></div>
+  <div class="avatar-holder">
+    <img src="../../../public/img/logo.jpg" alt="">
+  </div>
+  <form id="updateForm"> 
+    <div class="name">
+        <h5><label for="username"><?php echo $_SESSION['username'] ?></label></h5><br><br>
+        <a><label for="name">Nama:</label></a>
+            <input type="text" id="name" name="name" value="<?php include "../../../api/profile/get_name.php"?>" required>
+            <br><br>
+        <a><label for="email">Email:</label></a>
+            <input type="email" id="email" name="email" value="<?php include "../../../api/profile/get_email.php"?>" required>
+            <br><br>
+        <a><label for="old-password">Old-pw:</label></a>
+            <input type="password" id="old-password" name="old-password" value="">
+            <br><br>
+        <a><label for="new-password">New-pw:</label></a>
+            <input type="password" id="new-password" name="new-password" value="">
+            <br><br>
+    </div>
+    <div class="button">
+      <button type="submit" class="btn">Save</button>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
