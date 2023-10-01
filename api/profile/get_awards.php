8fa91396f3ba681253fc5a66697b77122aaa3b9e
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-01 17:53:15 +0700

    feat: get_awards done

--- api/profile/get_awards.php
+++ api/profile/get_awards.php
@@ -1,2 +1,12 @@
 <?php
-echo 29;
+// session_start();
+include_once __DIR__."/../session.php";
+
+$conn = Database::getInstance();
+$username = $_SESSION['username'];
+
+$sql = "SELECT total_achievement FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
+echo $row['total_achievement'];
