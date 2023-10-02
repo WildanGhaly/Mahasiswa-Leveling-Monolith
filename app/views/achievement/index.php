134cf84a3a57f8cc29a1b928aba67a9d673a02df
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-02 13:15:55 +0700

    feat: add achievements dummy pagination

---
+++ app/views/achievement/index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Achievement</title>
+    <link rel="stylesheet" href="../../../public/css/achievement.css">
+    <script src="../../../public/js/achievement.js"></script>
+</head>
+
+<body>
+    <?php include "../dashboard/index.php" ?>
+    <div class="pagination">
+        <ul class="achievement">
+            <li><a href="#" class="prev">
+                <i class="fa fa-chevron-left"></i>
+                Previous
+                </a>
+            </li>
+            <li><a href="#">1</a></li>
+            <li> <a href="#">2</a></li>
+            <li> <a href="#">3</a></li>
+            <li> <a href="#">4</a></li>
+            <li> <a href="#" class="active">5</a></li>
+            <li> <a href="#">6</a></li>
+            <li> <a href="#">7</a></li>
+            <li><a href="#" class="next"> Next 
+                <i class="fa fa-chevron-right"></i>
+            </a></li>
+        </ul>
+    </div>
+</body>
--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -14,7 +14,7 @@
             <li><a href="#">Challenges</a></li>
             <li><a href="#">Hall of Fame</a></li>
             <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+            <li><a href="../achievement">Achievement</a></li>
             <li><a href="../user-profile">ProfileTemp</a></li>
             <li><a href="#" id="logoutLink">LogoutTemp</a><li>
         </ul>
