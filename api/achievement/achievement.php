3303a8bfb9edebb5eedd161bd49128e0163de3bf
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-04 05:49:29 +0700

    feat: add dynamic dummy pagination achievement

--- api/achievement/achievement.php
+++ api/achievement/achievement.php
@@ -1,17 +1,39 @@
 <?php
-// load_achievement.php
-$perPage = 5; // Jumlah item per halaman
-$page = $_GET['page'];
+include_once __DIR__."/../session.php";
+
+$conn = Database::getInstance();
+$perPage = 10;
+
+$limit = isset($_POST["limit-records"]) ? $_POST["limit-records"] : $perPage;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$start = ($page - 1) * $limit;
+$result = $conn->query("SELECT * FROM achievement LIMIT $start, $limit");
+$customers = $result->fetch_all(MYSQLI_ASSOC);
+
+$result1 = $conn->query("SELECT count(id) AS id FROM achievement");
+$custCount = $result1->fetch_all(MYSQLI_ASSOC);
+$total = $custCount[0]['id'];
+$pages = ceil( $total / $limit );
+
+$Previous = $page - 1;
+$Next = $page + 1;
+
+$achievementList = '<tr>';
+foreach ($customers as $item) {
+    $achievementList .= '<td>'.$item["id"].'</td>   ';
+    $achievementList .= '<td>'.$item["name"].'</td><br>';
+}
+$achievementList .= '</tr>';
 
 // Data dummy (misalnya dari database)
 $dummyData = range(($page - 1) * $perPage + 1, $page * $perPage);
 
 // Buat daftar item achievement
-$achievementList = '<ul>';
-foreach ($dummyData as $item) {
-    $achievementList .= "<li>Achievement $item</li>";
-}
-$achievementList .= '</ul>';
+// $achievementList = '<ul>';
+// foreach ($dummyData as $item) {
+//     $achievementList .= "<li>Achievement $item</li>";
+// }
+// $achievementList .= '</ul>';
 
 // Buat tombol pagination
 $totalPages = 7;
@@ -24,7 +46,8 @@ $paginationButtons .= '</ul>';
 
 $response = [
     'achievementList' => $achievementList,
-    'paginationButtons' => $paginationButtons
+    'paginationButtons' => $paginationButtons,
+    'get' => $_GET['page']
 ];
 
 header('Content-Type: application/json');
--- public/js/achievement.js
+++ public/js/achievement.js
@@ -10,16 +10,26 @@ document.addEventListener('DOMContentLoaded', function () {
         xhr.open('GET', `${SERVER_PATH}achievement/achievement.php?page=${page}`, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(this.responseText.get);
                 const response = JSON.parse(xhr.responseText);
-                // achievementList.innerHTML = response.achievementList;
+                achievementList.innerHTML = response.achievementList;
                 paginationButtons.innerHTML = response.paginationButtons;
             }
         };
         xhr.send();
     }
 
-    loadAchievementPage(1); // Load halaman pertama saat halaman dimuat
-    console.log("loadAchievementPage(1) called");
+    var urlParams = new URLSearchParams(window.location.search);
+
+    // Mendapatkan nilai dari parameter dengan nama tertentu
+    var nilaiParameter = urlParams.get('page');
+
+    // Menggunakan nilai parameter
+    console.log(nilaiParameter);
+
+    nilaiParameter ? loadAchievementPage(nilaiParameter) : loadAchievementPage(1);
+
+    console.log(`loadAchievementPage(${nilaiParameter})`);
     // Menggunakan event delegation untuk menangani klik tombol pagination
     paginationButtons.addEventListener('click', function (e) {
         if (e.target.classList.contains('pagination-link')) {
