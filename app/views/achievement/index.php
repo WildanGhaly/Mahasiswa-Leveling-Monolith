2f8f87c535fb40f57dcad71fdcf2eed64213f408
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-05 20:16:29 +0700

    feat: add dummy for sort achievement

--- app/views/achievement/index.php
+++ app/views/achievement/index.php
@@ -16,22 +16,40 @@
         <br><br>
         <div class="search-container">
             <input type="text" class="searchInput" id="searchInput" placeholder="Search...">
-            <div class="filter-container">
-                <select class="filter-difficulty" id="filter-difficulty">
-                    <option value="semua">Semua</option>
-                    <option value="Beginner">Beginner</option>
-                    <option value="Intermediate">Intermediate</option>
-                    <option value="Advanced">Advanced</option>
-                </select>
-                <select class="filter-type" id="filter-type">
-                    <option value=0>Semua</option>
-                    <option value=1>Cardio Achievements</option>
-                    <option value=2>Strength Achievements</option>
-                    <option value=3>Flexibility and Balance Achievements</option>
-                    <option value=4>Variety and Endurance Achievements</option>
-                    <option value=5>Mindfulness and Wellness Achievements</option>
-                    <option value=6>General Fitness Achievements</option>
-                </select>
+            <div class="filter-sort-container">
+                <div class="filter-container">
+                    <h4>Filter: </h4>
+                    <select class="filter-difficulty" id="filter-difficulty">
+                        <option value="semua">Difficulty</option>
+                        <option value="Beginner">Beginner</option>
+                        <option value="Intermediate">Intermediate</option>
+                        <option value="Advanced">Advanced</option>
+                    </select>
+                    <select class="filter-type" id="filter-type">
+                        <option value=0>Category</option>
+                        <option value=1>Cardio Achievements</option>
+                        <option value=2>Strength Achievements</option>
+                        <option value=3>Flexibility and Balance Achievements</option>
+                        <option value=4>Variety and Endurance Achievements</option>
+                        <option value=5>Mindfulness and Wellness Achievements</option>
+                        <option value=6>General Fitness Achievements</option>
+                    </select>
+                </div>
+                <div class="sort-container">
+                    <h4>Sort:   </h4>
+                    <select class="sort-by" id="sort-by">
+                        <option value="">Sort</option>
+                        <option value="id">ID</option>
+                        <option value="name">Name</option>
+                        <option value="difficulty">Difficulty</option>
+                        <option value="category">Category</option>
+                    </select>
+                    <select class="sort-type" id="sort-type">
+                        <option value="">Sort-Type</option>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
+                    </select>
+                </div>
             </div>
         </div>
         <br><br>
