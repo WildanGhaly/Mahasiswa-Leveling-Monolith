efd750be3c71acff81cf6e90f371aca38b4955f0
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-06 15:24:43 +0700

    feat: change register page to the new one

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
   <title>Registration</title>
-  <link rel="stylesheet" type="text/css" href="../../../public/css/register.css">
+  <link rel="stylesheet" type="text/css" href="../../../public/css/login.css">
   <script src="../../../public/js/register.js"></script>
 </head>
 <body>
-  <h1>Registration</h1>
+  <!-- <h1>Registration</h1>
   <form id="registerForm">
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" required>
@@ -27,6 +27,43 @@
     
     <button type="submit">Daftar</button>
     
-  </form>
+  </form> -->
+  <section>
+	<?php for ($i = 0; $i < 260; $i++) { ?>
+		<span></span>
+	<?php } ?>
+   <div class="signin"> 
+    <div class="content"> 
+     <h2>Sign Up</h2> 
+     <form id="registerForm" class="form">
+        <div class="inputBox" id="inputBox"> 
+          <input id="username" type="text" aria-labelledby="inputBox" required> <i>Username</i> 
+        </div> 
+        <span id="usernameError"></span>
+        <span id="usernameError2"></span>
+        <div class="inputBox" id="inputBox"> 
+          <input id="email" type="text" class="label" aria-labelledby="inputBox" required> <i>Email</i>
+        </div>
+        <span id="emailError"></span>
+        <div class="inputBox"> 
+          <input id="password" type="password" aria-labelledby="inputBox" required> <i>Password</i> 
+        </div> 
+        <span id="passwordError"></span>
+        <div class="links"> 
+          <a href="../login/">Already has an account?</a> 
+          <a href="../login">Sign In</a> 
+        </div> 
+        <div class="inputBox"> 
+          <input type="submit" value="Register"> 
+        </div> 
+        <div class="inputBox"> 
+          <div class="back">
+            <a class="backbtn" href="../landing">Back</a>
+          </div>
+        </div>
+      </form>
+    </div> 
+   </div> 
+  </section>
 </body>
 </html>
