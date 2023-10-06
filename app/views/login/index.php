63010917f1f35b3308a438dc8e2b1d58ba8a2f97
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-06 14:31:20 +0700

    style: change the form of the login

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>Login</title>
   <link rel="stylesheet" type="text/css" href="../../../public/css/login.css">
   <script src="../../../public/js/login.js"></script>
 </head>
 <body>
-  <h1>Login</h1>
-  <form id="loginForm">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-
-    <a href="../register">Don't have an account?</a>
-    <br><br>
-    
-    <button type="submit">Login</button>
-  </form>
+  <section>
+	<?php for ($i = 0; $i < 260; $i++) { ?>
+		<span></span>
+	<?php } ?>
+   <div class="signin"> 
+    <div class="content"> 
+     <h2>Sign In</h2> 
+     <form id="loginForm" class="form">
+        <div class="inputBox" id="inputBox"> 
+          <input id="username" type="text" aria-labelledby="inputBox" required> <i>Username</i> 
+        </div> 
+          <div class="inputBox"> 
+        <input id="password" type="password" aria-labelledby="inputBox" required> <i>Password</i> 
+          </div> 
+        <div class="links"> 
+          <a href="#">Forgot Password</a> 
+          <a href="../register/">Signup</a> 
+        </div> 
+        <div class="inputBox"> 
+          <input type="submit" value="Login"> 
+        </div> 
+        <div class="inputBox"> 
+          <div class="back">
+            <a class="backbtn" href="../landing">Back</a>
+          </div>
+        </div>
+      </form>
+    </div> 
+   </div> 
+  </section>
 </body>
 </html>
