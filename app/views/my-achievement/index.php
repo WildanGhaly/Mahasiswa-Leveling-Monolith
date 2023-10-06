e93a6303543af4e9d06c59b84f5555f4d3e3b0c4
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-06 07:13:24 +0700

    style: add width for sort-filter

--- app/views/my-achievement/index.php
+++ app/views/my-achievement/index.php
@@ -28,19 +28,26 @@ if (!isset($_SESSION['username'])) {
                 <div class="filter-container">
                     <h4>Filter: </h4>
                     <select class="filter-difficulty" id="filter-difficulty">
-                        <option value="semua">Difficulty</option>
-                        <option value="Beginner">Beginner</option>
-                        <option value="Intermediate">Intermediate</option>
-                        <option value="Advanced">Advanced</option>
+                        <option value=0>Year</option>
+                        <option value=2020>2020</option>
+                        <option value=2021>2021</option>
+                        <option value=2022>2022</option>
+                        <option value=2023>2023</option>
                     </select>
                     <select class="filter-type" id="filter-type">
-                        <option value=0>Category</option>
-                        <option value=1>Cardio Achievements</option>
-                        <option value=2>Strength Achievements</option>
-                        <option value=3>Flexibility and Balance Achievements</option>
-                        <option value=4>Variety and Endurance Achievements</option>
-                        <option value=5>Mindfulness and Wellness Achievements</option>
-                        <option value=6>General Fitness Achievements</option>
+                        <option value=0>Month</option>
+                        <option value=1>January</option>
+                        <option value=2>February</option>
+                        <option value=3>March</option>
+                        <option value=4>April</option>
+                        <option value=5>May</option>
+                        <option value=6>June</option>
+                        <option value=7>July</option>
+                        <option value=8>August</option>
+                        <option value=9>September</option>
+                        <option value=10>October</option>
+                        <option value=11>November</option>
+                        <option value=12>December</option>
                     </select>
                 </div>
                 <div class="sort-container">
