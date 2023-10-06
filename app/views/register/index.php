d53b70081db652f9b47b19ba106ac7e63b3a5f9a
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-06 15:25:54 +0700

    rm: remove unused variable on register

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -6,28 +6,6 @@
   <script src="../../../public/js/register.js"></script>
 </head>
 <body>
-  <!-- <h1>Registration</h1>
-  <form id="registerForm">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-    <p>5-20 karakter</p>
-    <p>Hanya boleh terdiri dari huruf, angka, dan underscore</p>
-    <span id="usernameError"></span>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-    <span id="emailError"></span>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <span id="passwordError"></span>
-
-    <a href="../login">Already has an account?</a>
-    <br><br>
-    
-    <button type="submit">Daftar</button>
-    
-  </form> -->
   <section>
 	<?php for ($i = 0; $i < 260; $i++) { ?>
 		<span></span>
