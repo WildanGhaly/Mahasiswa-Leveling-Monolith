07685c6cdfc936a6ec0042766b06698b3218c319
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 19:49:20 +0700

    feat: add confirm on js b4 call BE (CHALLENGE)

--- public/js/challenge-claim.js
+++ public/js/challenge-claim.js
@@ -1,6 +1,9 @@
 document.write('<script src="../../../public/js/api.js"></script>');
 
 function claimFunction(user_id, quest_id){
+    if (!confirm("Claim this quest?")) {
+        return;
+    }
     console.log("claimFunction() called");
     var url = `${SERVER_PATH}challenge/claim.php`;
     const xhr = new XMLHttpRequest();
@@ -10,6 +13,7 @@ function claimFunction(user_id, quest_id){
         if (xhr.readyState === 4 && xhr.status === 200) {
             console.log(this.responseText);
             // const response = JSON.parse(xhr.responseText);
+            window.location.reload();
         }
     }; 
     const data = `user_id=${encodeURIComponent(user_id)}&quest_id=${encodeURIComponent(quest_id)}`
@@ -17,15 +21,19 @@ function claimFunction(user_id, quest_id){
 }
 
 function unclaimFunction(user_id, quest_id){
+    if (!confirm("Unclaim this quest?")) {
+        return;
+    }
     console.log("unclaimFunction() called");
     var url = `${SERVER_PATH}challenge/unclaim.php`;
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
+    xhr.open('DELETE', url, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             console.log(this.responseText);
             // const response = JSON.parse(xhr.responseText);
+            window.location.reload();
         }
     }; 
     const data = `user_id=${encodeURIComponent(user_id)}&quest_id=${encodeURIComponent(quest_id)}`
