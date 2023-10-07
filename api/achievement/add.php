5477887cbacbb90c4d818d1b4521177b7a4accc5
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 12:43:34 +0700

    feat: add error handling for add achievement (DB)

--- api/achievement/add.php
+++ api/achievement/add.php
@@ -15,8 +15,23 @@ $difficulty     = $_POST['difficulty'];
 $group_id       = $_POST['type'];
 
 $conn = Database::getInstance();
+
+$check = "SELECT * FROM achievement WHERE name = '$name'";
+$resultcheck = $conn->query($check);
+if ($resultcheck->num_rows > 0) {
+    http_response_code(409);
+    echo "Nama achievement sudah ada";
+    exit();
+}
+
 $sql = "INSERT INTO achievement (name, description, threshold, difficulty, group_id) VALUES ('$name', '$description', '$threshold', '$difficulty', '$group_id')";
 $result = $conn->query($sql);
 
-echo $sql;
+if (!$result) {
+    http_response_code(500);
+    echo "Error: ".$conn->error;
+    exit();
+}
+
+echo "success";
 ?>
