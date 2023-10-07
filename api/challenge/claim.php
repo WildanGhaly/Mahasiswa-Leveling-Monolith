f17ebf70d16c91721f2e0a52087b13b9f00f8900
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 19:48:26 +0700

    feat: add claim and unclaim backend for CHALLENGE

--- api/challenge/claim.php
+++ api/challenge/claim.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello World!";
+include_once __DIR__."/../session.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    parse_str(file_get_contents('php://input'), $_POST);
+    $user_id    = $_POST['user_id'];
+    $quest_id   = $_POST['quest_id'];
+
+    $conn = Database::getInstance();
+
+    $sql = "INSERT INTO user_quest (user_id, quest_id) VALUES ($user_id, $quest_id)";
+    $result = $conn->query($sql);
+
+    echo $sql;
+
+} else {
+    http_response_code(405);
+    echo "Metode HTTP yang tidak diizinkan";
+}
--- api/challenge/unclaim.php
+++ api/challenge/unclaim.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hello World2!";
+include_once __DIR__."/../session.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    parse_str(file_get_contents('php://input'), $_DELETE);
+    $user_id = $_DELETE['user_id'];
+    $quest_id = $_DELETE['quest_id'];
+
+    $conn = Database::getInstance();
+
+    $sql = "DELETE FROM user_quest WHERE user_id = $user_id AND quest_id = $quest_id";
+    $result = $conn->query($sql);
+    
+    echo $sql;
+} else {
+    http_response_code(405);
+    echo "Metode HTTP yang tidak diizinkan";
+}
