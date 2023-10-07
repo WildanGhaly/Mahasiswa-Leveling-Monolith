881fe1d6f78510508364a2dbb3392b6e25d8fdec
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 12:28:14 +0700

    feat: change popup location (achievement-admin)

--- app/views/admin-achievement/index.php
+++ app/views/admin-achievement/index.php
@@ -20,37 +20,12 @@ if (!isset($_SESSION['username'])) {
 <body>
     <?php include "../dashboard/index.php" ?>
     <div class="achievement-container">
-        <div id="achievement-popup" class="achievement-popup">
-            <h2>Edit</h2>
-            <p>Change the achievement information</p>
-            <input type="text" id="achievement-name-input" class="achievement-name-input" placeholder="Name">
-            <br>
-            <textarea id="achievement-description-input" name="achievement-description-input" rows="5" cols="50" placeholder="Description"></textarea>
-            <br>
-            <textarea id="achievement-threshold-input" name="achievement-threshold-input" rows="5" cols="50" placeholder="Threshold"></textarea>
-            <br>
-            <select id="achievement-difficulty-input" name="achievement-difficulty-input">
-                <option value="Beginner">Beginner</option>
-                <option value="Intermediate">Intermediate</option>
-                <option value="Advanced">Advanced</option>
-            </select>
-            <br>
-            <select id="achievement-type-input" name="achievement-type-input">
-                <option value=1>Cardio Achievements</option>
-                <option value=2>Strength Achievements</option>
-                <option value=3>Flexibility and Balance Achievements</option>
-                <option value=4>Variety and Endurance Achievements</option>
-                <option value=5>Mindfulness and Wellness Achievements</option>
-                <option value=6>General Fitness Achievements</option>
-            </select>
-            <br>
-            <button id="save-achievement-popup" class="save-achievement-popup">Save</button>
-            <button id="close-achievement-popup" class="close-achievement-popup">Cancel</button>
-        </div>
+        <? include "edit.php" ?> 
+        <? include "add.php" ?>
         <div id="overlay" class="overlay"></div>
         <h1><b>ACHIEVEMENT-ADMIN</b></h1>
         <button class="btn" onclick="window.location.href='../achievement'"><b>ACHIEVEMENT</b></button>
-        <button class="btn" id="add-achievement" onclick="window.location.href='#'"><b>ADD ACHIEVEMENT</b></button>
+        <button class="btn" id="add-achievement" onclick=addFunction()><b>ADD ACHIEVEMENT</b></button>
         <button class="btn" onclick="window.location.href='../my-achievement'"><b>MY ACHIEVEMENT</b></button>
         <div class="status" id="status"></div>
         <br><br>
