8febb3e724e9c8fde5553e8e7649be7e50864b7f
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 12:41:41 +0700

    feat: add "add achievement" BE

--- api/achievement/add.php
+++ api/achievement/add.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello World!";
+include_once __DIR__."/../session.php";
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo "Metode HTTP yang tidak diizinkan";
+    exit();
+}
+
+$name           = $_POST['name'];
+$description    = $_POST['description'];
+$threshold      = $_POST['threshold'];
+$difficulty     = $_POST['difficulty'];
+$group_id       = $_POST['type'];
+
+$conn = Database::getInstance();
+$sql = "INSERT INTO achievement (name, description, threshold, difficulty, group_id) VALUES ('$name', '$description', '$threshold', '$difficulty', '$group_id')";
+$result = $conn->query($sql);
+
+echo $sql;
+?>
