ffe16c89612bac04f044d8b93d1fb857b92b2b59
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 12:29:09 +0700

    feat: add dummy for add achievement

--- public/js/achievement-crud.js
+++ public/js/achievement-crud.js
@@ -49,6 +49,58 @@ function editFunction(id){
         document.getElementById("achievement-description-input").value = "";
         document.getElementById("achievement-threshold-input").value = "";
     });
+
+}
+
+function addFunction() {
+    document.getElementById("achievement-popup-add").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+
+    document.getElementById("save-achievement-popup-add").addEventListener("click", function() {
+        console.log("save-achievement-popup-add clicked");
+        document.getElementById("achievement-popup-add").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+        const name          = document.getElementById("achievement-name-input-add").value;
+        const description   = document.getElementById("achievement-description-input-add").value;
+        const threshold     = document.getElementById("achievement-threshold-input-add").value;
+        const difficulty    = document.getElementById("achievement-difficulty-input-add").value;
+        const type          = document.getElementById("achievement-type-input-add").value;
+
+        console.log(name);
+        console.log(description);
+        console.log(threshold);
+        console.log(difficulty);
+        console.log(type);
+
+        if (name || description || threshold) {
+            console.log("name or description or threshold is not empty");
+            var url = `${SERVER_PATH}achievement/add.php`;
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    console.log(this.responseText);
+                    // const response = JSON.parse(xhr.responseText);
+                }
+            }; 
+            const data = `name=${encodeURIComponent(name)}&description=${encodeURIComponent(description)}&threshold=${encodeURIComponent(threshold)}&difficulty=${encodeURIComponent(difficulty)}&type=${encodeURIComponent(type)}`
+            xhr.send(data);
+        }
+
+        document.getElementById("achievement-name-input-add").value = "";
+        document.getElementById("achievement-description-input-add").value = "";
+        document.getElementById("achievement-threshold-input-add").value = "";
+    });
+
+    document.getElementById("close-achievement-popup-add").addEventListener("click", function() {
+        console.log("close-achievement-popup-add clicked");
+        document.getElementById("achievement-popup-add").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+        document.getElementById("achievement-name-input-add").value = "";
+        document.getElementById("achievement-description-input-add").value = "";
+        document.getElementById("achievement-threshold-input-add").value = "";
+    });
 }
 
 function deleteFunction(id) {
