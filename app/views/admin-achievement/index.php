5ee96d53d074edb5ac3c985b37137cd4cc25faf5
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-07 11:15:12 +0700

    feat: add admin achievement page (FE)

---
+++ app/views/admin-achievement/index.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: ../login');
+    return;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Achievement-Admin</title>
+    <link rel="stylesheet" href="../../../public/css/achievement.css">
+    <link rel="stylesheet" href="../../../public/css/achievement-admin.css">
+    <script src="../../../public/js/achievement-admin.js"></script>
+    <script src="../../../public/js/achievement-crud.js"></script>
+</head>
+<body>
+    <?php include "../dashboard/index.php" ?>
+    <div class="achievement-container">
+        <div id="achievement-popup" class="achievement-popup">
+            <h2>Edit</h2>
+            <p>Change the achievement information</p>
+            <button id="save-achievement-popup" class="save-achievement-popup">Save</button>
+            <button id="close-achievement-popup" class="close-achievement-popup">Cancel</button>
+        </div>
+        <h1><b>ACHIEVEMENT-ADMIN</b></h1>
+        <button class="btn" onclick="window.location.href='../achievement'"><b>ACHIEVEMENT</b></button>
+        <button class="btn" id="add-achievement" onclick="window.location.href='#'"><b>ADD ACHIEVEMENT</b></button>
+        <button class="btn" onclick="window.location.href='../my-achievement'"><b>MY ACHIEVEMENT</b></button>
+        <div class="status" id="status"></div>
+        <br><br>
+        <div class="search-container">
+            <div class="full-search-container">
+                <input type="text" class="searchInput" id="searchInput" placeholder="Search...">
+                <select class="search-attribute" id="search-attribute" aria-labelledby="achievement-title">
+                    <option value="a.id">ID</option>
+                    <option value="a.name">Name</option>
+                    <option value="a.description">Description</option>
+                    <option value="a.threshold">Threshold</option>
+                </select>
+            </div>
+            <div class="filter-sort-container">
+                <div class="sort-container">
+                    <h2>Sort:   </h2>
+                    <select class="sort-by" id="sort-by">
+                        <option value="default">Sort</option>
+                        <option value="id">ID</option>
+                        <option value="name">Name</option>
+                        <option value="description">Description</option>
+                        <option value="threshold">Threshold</option>
+                    </select>
+                    <select class="sort-type" id="sort-type">
+                        <option value="default">Sort-Type</option>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="filter-box">
+            <select class="page-limit" id="page-limit">
+                <option value=5>5</option>
+                <option value=10>10</option>
+                <option value=15>15</option>
+                <option value=20>20</option>
+            </select>
+        </div>
+        <table class="achievement-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Threshold</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody class="achievement-list" id="achievement-list">
+                <!-- Data achievement di sini -->
+            </tbody>
+        </table>
+        
+        <div class="pagination">
+            <div id="pagination-buttons">
+                <!-- Tombol pagination akan dimuat di sini menggunakan AJAX -->
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
---
+++ public/js/achievement-admin.js
@@ -0,0 +1,108 @@
+document.write('<script src="../../../public/js/api.js"></script>');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const achievementList = document.getElementById('achievement-list');
+    const paginationButtons = document.getElementById('pagination-buttons');
+    var urlParams = new URLSearchParams(window.location.search);
+    let timeout;
+
+    var page = urlParams.get("page");
+    
+    var achievementLimitCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('achievement-admin-limit='));
+    var limit = achievementLimitCookie ? achievementLimitCookie.split('=')[1] : 5;
+    document.getElementById('page-limit').value = limit;
+
+    var achievementSearchCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('achievement-admin-search='));
+    var search = achievementSearchCookie ? achievementSearchCookie.split('=')[1] : "";
+    document.getElementById('searchInput').value = search;
+
+    var achievementSearchAttributeCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('achievement-admin-search-type='));
+    var searchType = achievementSearchAttributeCookie ? achievementSearchAttributeCookie.split('=')[1] : "name";
+    document.getElementById('search-attribute').value = searchType;
+
+    var achievementSortCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('achievement-admin-sort='));
+    var sort = achievementSortCookie ? achievementSortCookie.split('=')[1] : "default";
+    document.getElementById('sort-by').value = sort;
+
+    var achievementOrderCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('achievement-admin-order='));
+    var order = achievementOrderCookie ? achievementOrderCookie.split('=')[1] : "asc";
+    document.getElementById('sort-type').value = order;
+
+    console.log(limit);
+
+    page = page ? page : 1;
+
+
+    function loadAchievementPage(page) {
+        var url = `${SERVER_PATH}achievement/achievement-admin.php?page=${page}`;
+        if (limit && limit !== "null" && limit !== "undefined") {
+            url += `&limit=${limit}`;
+        }
+        console.log(url);
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(this.responseText);
+                const response = JSON.parse(xhr.responseText);
+                achievementList.innerHTML = response.achievementList;
+                paginationButtons.innerHTML = response.paginationButtons;
+            }
+        };
+        xhr.send();
+    }
+
+    document.getElementById("page-limit").addEventListener("change", function () {
+        limit = this.value;
+        if (!limit || limit === "null" || limit === "undefined") {
+            limit = 5;
+        }
+        document.cookie = `achievement-admin-limit=${limit}; path=/`
+        loadAchievementPage(1);
+    });
+
+    document.getElementById("searchInput").addEventListener("keyup", function () {
+        search = this.value;
+        clearTimeout(timeout);
+
+        if (!search || search === "null" || search === "undefined") {
+            search = "";
+        }
+
+        timeout = setTimeout(function () {
+            document.cookie = `achievement-admin-search=${search}; path=/`
+            loadAchievementPage(1);
+        }, 500);
+    });
+
+    document.getElementById("search-attribute").addEventListener("change", function () {
+        searchType = this.value;
+        if (!searchType || searchType === "null" || searchType === "undefined") {
+            searchType = "a.name";
+        }
+        document.cookie = `achievement-admin-search-type=${searchType}; path=/`
+        loadAchievementPage(1);
+    });
+
+    document.getElementById("sort-by").addEventListener("change", function () {
+        sort = this.value;
+        if (!sort || sort === "null" || sort === "undefined" || sort === "default") {
+            sort = "default";
+        }
+        document.cookie = `achievement-admin-sort=${sort}; path=/`
+        loadAchievementPage(1);
+    });
+
+    document.getElementById("sort-type").addEventListener("change", function () {
+        order = this.value;
+        if (!order || order === "null" || order === "undefined" || order === "default") {
+            order = "default";
+        }
+        document.cookie = `achievement-admin-order=${order}; path=/`
+        loadAchievementPage(1);
+    });
+
+    loadAchievementPage(page);
+});
+
+
