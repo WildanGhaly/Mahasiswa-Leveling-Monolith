c310e6cbf3254fdb40b59cebbf61aa27ff08ae23
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-08 10:52:46 +0700

    feat: add responsive setup SIDEBAR

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -5,10 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Menu</title>
     <link rel="stylesheet" href="../../../public/css/dashboard.css">
-    <script src="../../../public/js/dashboard.js"></script> <!-- Sementara untuk test -->
+    <script src="../../../public/js/dashboard.js"></script>
   </head>
 <body>
+    <button class="toggle-sidebar" id="toggle-sidebar">
+        &#9776; 
+    </button>
+    
     <div class="sidebar">
+      <button class="close-sidebar" id="close-sidebar">
+        &#10006;
+      </button>
         <h2>Darmodar</h2>
         <ul>
             <li><a href="../challenge/">Challenges</a></li>
@@ -16,7 +23,7 @@
             <li><a href="../collection">Collection</a></li>
             <li><a href="../achievement">Achievement</a></li>
             <li><a href="../user-profile">ProfileTemp</a></li>
-            <li><a href="#" id="logoutLink">LogoutTemp</a><li>
+            <li><a href="#" id="logoutLink">LogoutTemp</a></li>
         </ul>
     </div>
 
